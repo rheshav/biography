9a5fac8e1bf3594376308ed1b6e2e1cdb0079c26
rheshav <rhesa.havilah@gmail.com>
2021-12-21 23:17:08 +0700

    add simple condition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+// condition sederhana
+let nilai = prompt(`masukkan nilai `);
+
+if (nilai >= 90 && nilai <= 100) {
+  alert(`lulus predikat sangat baik`);
+} else if (nilai >= 80 && nilai < 90) {
+  alert(`lulus predikat baik`);
+} else if (nilai >= 70 && nilai < 80) {
+  alert(`lulus predikat cukup`);
+} else if (nilai < 70 && nilai >= 0) {
+  alert(`maaf nilai Anda belum memenuhi KKM`);
+} else {
+  alert(`nilai tidak valid, silahkan masukkan nilai antara 1-100`);
+}
+
+// end of condition sederhana
+
 function App() {
   return (
     <div className="App">
@@ -9,12 +26,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
