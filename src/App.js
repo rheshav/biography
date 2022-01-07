635fa77c173e4ca76dfbc6d314c98cc17c681487
rheshav <rhesa.havilah@gmail.com>
2022-01-07 21:28:20 +0700

    udah looping tinggal scoring

--- src/App.js
+++ src/App.js
@@ -73,46 +73,49 @@ function App() {
 export default App;
 
 //game tebak angka
-alert`Silahkan bermain`;
+alert(`Silahkan bermain`);
 
-let player1 = prompt`Player 1 Silahkan masukkan Angka dari 1-2`;
-let player2 = prompt`Player 2 Silahkan masukkan Angka dari 1-2`;
-let acak = Math.floor(Math.random() * 2) + 1;
+let ronde = () => {
+  for (let i = 1; i <= 5; i++) {
+    alert('ronde ke ' + i);
+    let player1 = prompt(`Player 1 Silahkan masukkan Angka antara 1-2`);
+    let player2 = prompt(`Player 2 Silahkan masukkan Angka antara 1-2`);
+    let acak = Math.floor(Math.random() * 2) + 1;
 
-console.log(acak); //untuk cek value acak
-console.log(`angka dari Player 1 = ` + player1);
+    console.log(acak); //untuk cek value acak
+    console.log(`angka dari Player 1 = ` + player1); //cek value player 1
 
-//kondisi permainan
-if (player1 == acak && player1 != player2) {
-  alert`Player 1 Win`;
-} else if (player2 == acak && player1 != player2) {
-  alert`Player 2 Win`;
-} else if (player1 == player2) {
-  alert`Tebakan tidak boleh sama`;
-} else if (player1 <= 0) {
-  alert`tebakan tidak boleh kurang dari 0`;
-} else if (player2 <= 0) {
-  alert`tebakan tidak boleh kurang dari 0`;
-} else if (player1 > 2) {
-  alert`tebakan tidak boleh lebih dari 2`;
-} else if (player2 > 2) {
-  alert`tebakan tidak boleh lebih dari 2`;
-} else {
-  alert`silahkan masukkan nilai yang valid`;
-}
+    //kondisi permainan
+    if (player1 == acak && player1 != player2) {
+      alert(`Player 1 Win`);
+    } else if (player2 == acak && player1 != player2) {
+      alert(`Player 2 Win`);
+    } else if (player1 == player2) {
+      alert(`Tebakan tidak boleh sama`);
+    } else if (player1 <= 0) {
+      alert(`tebakan tidak boleh kurang dari 0`);
+    } else if (player2 <= 0) {
+      alert(`tebakan tidak boleh kurang dari 0`);
+    } else if (player1 > 2) {
+      alert(`tebakan tidak boleh lebih dari 2`);
+    } else if (player2 > 2) {
+      alert(`tebakan tidak boleh lebih dari 2`);
+    } else {
+      alert(`silahkan masukkan nilai yang valid`);
+    }
 
-alert(`Player 1 memasukkan angka  ${player1}
-Player 2 memasukkan angka ${player1}
+    alert(`Player 1 memasukkan angka  ${player1}
+Player 2 memasukkan angka ${player2}
 Angka yang keluar ${acak}`);
-
+  }
+};
+ronde();
 // alert ('Player 1 memasukkan angka' + player1);
 // alert ('Player 1 memasukkan angka' + player1);
 
 //function untuk looping permainan
-let ronde = () => {
-  for (let i = 1; i <= 5; i++) {
-    alert('ronde ke ' + i);
-  }
-};
-
-ronde();
+// let ronde = () => {
+//   for (let i = 1; i <= 5; i++) {
+//     alert('ronde ke ' + i);
+//   }
+// };
