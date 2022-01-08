138c0fa779c7edd860d762743269f5dbf46a0e09
rheshav <rhesa.havilah@gmail.com>
2022-01-08 18:09:04 +0700

    back to default for next task

--- src/App.js
+++ src/App.js
@@ -1,59 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-/* control flow
-// task condition sederhana
-let nilai = prompt(`masukkan nilai `);
-
-if (nilai >= 90 && nilai <= 100) {
-  alert(`lulus predikat sangat baik`);
-} else if (nilai >= 80 && nilai < 90) {
-  alert(`lulus predikat baik`);
-} else if (nilai >= 70 && nilai < 80) {
-  alert(`lulus predikat cukup`);
-} else if (nilai < 70 && nilai >= 0) {
-  alert(`maaf nilai Anda belum memenuhi KKM`);
-} else {
-  alert(`nilai tidak valid, silahkan masukkan nilai antara 1-100`);
-}
-
-// end of condition sederhana
-
-// function declaration
-function deklarasi() {
-  console.log('deklarasi');
-}
-
-// function expression
-const expression = function () {
-  console.log('expression');
-};
-deklarasi();
-expression();
-console.log(deklarasi);
-console.log(expression);
-
-// Arrow function
-const arrow = () => {
-  console.log(`arrow`);
-};
-
-arrow();
-console.log(arrow());
-
-//Param
-function tambah(a, b) {
-  alert(`${a} + ${b} = ${a + b}`);
-}
-
-tambah(10, 20);
-
-const kali = function (a, b) {
-  alert(`${a} * ${b} = ${a * b}`);
-};
-kali(10, 20);
-*/
-
 function App() {
   return (
     <div className="App">
@@ -71,42 +18,3 @@ function App() {
 }
 
 export default App;
-
-//game tebak angka
-alert(`Silahkan bermain tebak angka antara 1 atau 2
-Permainan akan dilakukan dalam 3 ronde
-`);
-
-let ronde = () => {
-  for (let i = 1; i <= 3; i++) {
-    alert('ronde ke ' + i);
-    let player1 = prompt(`Player 1 Silahkan masukkan Angka antara 1-3`);
-    let player2 = prompt(`Player 2 Silahkan masukkan Angka antara 1-3`);
-    let acak = Math.floor(Math.random() * 3) + 1;
-
-    console.log(acak); //untuk cek value acak
-    console.log(`angka dari Player 1 = ` + player1); //cek value player 1
-
-    //kondisi permainan
-    if (player1 == acak && player1 != player2) {
-      alert(`Player 1 Win`);
-    } else if (player2 == acak && player1 != player2) {
-      alert(`Player 2 Win`);
-    } else if (player1 == player2) {
-      alert(`Tebakan tidak boleh sama`);
-    } else if (player1 != acak && player2 != acak) {
-      alert(`Tidak ada yang menebak dengan benar`);
-    } else if (player1 <= 0 || player2 <= 0) {
-      alert(`tebakan tidak boleh kurang dari 0`);
-    } else if (player1 > 3 || player2 > 3) {
-      alert(`tebakan tidak boleh lebih dari 3`);
-    } else {
-      alert(`silahkan masukkan nilai yang valid`);
-    }
-
-    alert(`Player 1 memasukkan angka  ${player1}
-Player 2 memasukkan angka ${player2}
-Angka yang keluar ${acak}`);
-  }
-};
-ronde();
