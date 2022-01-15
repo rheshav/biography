5b0554a73234800bbd258413f04ded2d755156e6
rheshav <rhesa.havilah@gmail.com>
2022-01-15 22:26:27 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
 import Lib from './Lib';
-import { Container, Row, Col } from 'react-bootstrap';
+
+// import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   let title = `Mini Library from Eduwork`;
   return (
-    <div>
-      <p>{title}</p>
+    <div style={{ marginLeft: 20 }}>
+      <p style={{ fontSize: 40 }}>{title}</p>
       <Lib></Lib>
     </div>
   );
--- src/Lib.js
+++ src/Lib.js
@@ -4,16 +4,16 @@ function Lib() {
   return (
     <Grid
       data={[
-        ['John', 'john@example.com'],
-        ['Mike', 'mike@gmail.com'],
-        ['John', 'john@example.com'],
-        ['Mike', 'mike@gmail.com'],
-        ['John', 'john@example.com'],
-        ['Mike', 'mike@gmail.com'],
-        ['John', 'john@example.com'],
-        ['Mike', 'mike@gmail.com'],
-        ['John', 'john@example.com'],
-        ['Mike', 'mike@gmail.com'],
+        ['John Lagrange', 'john@example.com'],
+        ['Mike Stewart', 'mike@gmail.com'],
+        ['John Kolvanski', 'john@example.com'],
+        ['Mike Palvonov', 'mike@gmail.com'],
+        ['John Kurberg', 'john@example.com'],
+        ['Mike Alan Sevinski', 'mike@gmail.com'],
+        ['John Deez', 'john@example.com'],
+        ['Mike Nutz', 'mike@gmail.com'],
+        ['John Yurivonne', 'john@example.com'],
+        ['Mike Wazowski', 'mike@gmail.com'],
       ]}
       columns={['Name', 'Email']}
     />
