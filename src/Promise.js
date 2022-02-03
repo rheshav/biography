b18e7f3ac32b1942f11bd235c26b8eca082d9050
rheshav <rhesa.havilah@gmail.com>
2022-02-03 21:49:34 +0700

    edit

---
+++ src/Promise.js
@@ -0,0 +1,16 @@
+// function Promises() {
+//   const promise = new Promise(function (resolve, reject) {
+//     if (true) {
+//       resolve('Promise Berhasil');
+//     } else {
+//       reject('Promise Gagal');
+//     }
+//   });
+
+//   return promise
+//     .then(function (result) {
+//       console.log(result);
+//     })
+//     .catch((error) => console.log(error));
+// }
+// export default Promises;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App, Hitung } from './App';
+// import { App, Hitung } from './App';
 import reportWebVitals from './reportWebVitals';
-import Render from './Callback';
+// import Render from './Callback';
+import Promises from './Promise';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Render />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<React.StrictMode></React.StrictMode>, document.getElementById('root'));
+const promise = new Promise(function (resolve, reject) {
+  if (false) {
+    resolve('Promise Berhasil');
+  } else {
+    reject('Promise Gagal');
+  }
+});
+
+promise
+  .then(function (result) {
+    console.log(result);
+  })
+  .catch((error) => console.log(error));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
